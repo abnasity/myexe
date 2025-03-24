cd2291c333c5de0fb156c5ec046d1d5e56a9c4d4
Kipngeno Abednego <abnasgit@gmail.com>
2025-03-24 22:58:39 +0300

    functions practice

---
+++ PYTHON/for_loop.py
@@ -0,0 +1,71 @@
+
+# USE CASES:
+
+
+#***apple and banana will be printed because the loop will break when it gets to cherry
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   print(x)
+#   if x == "banana":
+#     break
+
+
+# ***only apple will be printed because the loop will break when it gets to banana
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   if x == "banana":
+#     break
+#   print(x)
+  
+  
+#RANGE FUNCTION
+# For loop with range function
+# for x in range(6):
+#      print(x)
+
+# for x in range(2, 6):
+#   print(x)
+  
+#Increment the sequence with 3 (default is 1):
+# for x in range(2, 30, 3):
+#   print(x)
+
+
+  
+# x = ["fanta", "orage", "cocacola"]
+# for x in x:
+
+#   print (x)
+
+command = ""
+while command != "quit":
+    command = input("> ")
+    if command == "start":
+      if "started":
+          print("car already started")
+    else:
+        if "started" == True:
+         print("start the car.....")
+    
+        
+         
+        
+        elif command == "stop":
+            if  "stop":
+               print("car already stopped")
+            if "stop" == True:
+               print("stop the car.....")
+        
+         
+        
+            
+        elif command == "help":
+              print("""
+              start - start the car
+              stop - stop the car
+              quit - quits
+              """)
+    # elif command == "quit":
+    #     break
+    # else:
+    #     # print("park the car")
---
+++ PYTHON/functions.py
@@ -0,0 +1,18 @@
+#FUNCTIONS
+#A function is a block of code which only runs when it is called
+
+
+# def my_function(fname, lname):
+#   print(fname + " " + lname)
+
+# my_function("Emil", "Refsnes") 
+
+#Arguments also known as args are values passed when function is called
+#A variable is inside the function definition
+
+def greet(name):
+  print("Hello", name)
+  print("How do you do")
+  
+greet("Jack")
+  
