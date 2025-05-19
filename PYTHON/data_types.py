013c0c7b3eddd8cdda34352b7309498dbbc1249f
Kipngeno Abednego <abnasgit@gmail.com>
2025-05-19 12:24:48 +0300

    dict example

--- PYTHON/data_types.py
+++ PYTHON/data_types.py
@@ -8,3 +8,12 @@ float = 5.7
 bool = True, False
 char = 'A'
 string = "Hello"
+
+# Lets talk about dictionaries
+# In dictionaries we have both keys and values both in quotes
+Phone = {
+    "model": "Samsung",
+    "Processor" : "Mediatek",
+    "Android version" : "15",
+    "Specs" : "Storage 256 Ram 8gb"
+}
