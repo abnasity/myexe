f8ca153469b967c6cd16f5951063a55317d7f0d8
Kipngeno Abednego <abnasgit@gmail.com>
2025-05-25 12:15:53 +0300

    another example

--- PYTHON/set.py
+++ PYTHON/set.py
@@ -1,3 +1,5 @@
 # stores a sequence of items
 # uses carli braces just like the dictionary but doesnt have key value pairs
 cars = {"G-wagon", "Mercedes", "Toyota", "Probox", "Range rover"}
+
+cars = {"G-wagon", 23, 34.5, "Toyota", "Range rover"} #can contain numbers
